faaab4952f8c66522bc9ed99c8715afe583a331d
HuangKY <yao@gyounoMacBook-Pro.local>
2020-03-27 14:02:15 +0900

    Add description and and reaggrange the structure.

---
+++ NLP/NLP_Practices/utils/cleansing.py
@@ -0,0 +1,26 @@
+import re
+
+def test_001(word):
+    '''
+    <INPUT>
+    word: STRING
+    
+    <OUTPUT>
+    
+    <INTRO>
+    
+    '''
+    return ree.findall(r"n\'t$|not|n\'$", word)
+
+
+def test_002(word):
+    '''
+    <INPUT>
+    word: STRING
+    
+    <OUTPUT>
+    
+    <INTRO>
+    
+    '''
+    return re.findall(r"n\'t$|not|n\'$", word)
